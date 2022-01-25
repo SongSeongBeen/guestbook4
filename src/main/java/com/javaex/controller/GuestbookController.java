01a1d7a2430e9536bff214f5133b074e7bc6b456
송성빈 <aktm1004@naver.com>
2022-01-25 17:57:34 +0900

    guestbook4

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -15,55 +15,55 @@ import com.javaex.vo.GuestbookVo;
 @Controller
 @RequestMapping(value="/book")
 public class GuestbookController {
-	//필드
-	@Autowired
-	private GuestbookDao guestbookDao;
-	private GuestbookVo guestbookVo;
-	//생성자
-	
-	//메소드gs
-	
-	//메소드 일반
-	
+		//필드
+		@Autowired
+		private GuestbookDao guestbookDao;
+		private GuestbookVo guestbookVo;
+		//생성자
+		
+		//메소드gs
+		
+		//메소드 일반
+		
 //리스트	
-	@RequestMapping(value = "/gList", method= {RequestMethod.GET, RequestMethod.POST})
-	public String gList(Model model) {
-			System.out.println("GuestbookController=addList");
-		
-		List<GuestbookVo> gList = guestbookDao.getList();
-		System.out.println(gList.toString());
-		
-		model.addAttribute("gList", gList);
+		@RequestMapping(value = "/gList", method= {RequestMethod.GET, RequestMethod.POST})
+		public String gList(Model model) {
+				System.out.println("GuestbookController=addList");
 			
-		return "addList";
-	}
-
+			List<GuestbookVo> gList = guestbookDao.getList();
+			System.out.println(gList.toString());
+			
+			model.addAttribute("gList", gList);
+				
+			return "addList";
+		}
+	
 //리스트입력-확인
-	@RequestMapping(value = "/add", method= {RequestMethod.GET, RequestMethod.POST})
-	public String add(@ModelAttribute GuestbookVo guestboookVo) {
-		System.out.println("GuestbookController=add");
+		@RequestMapping(value = "/add", method= {RequestMethod.GET, RequestMethod.POST})
+		public String add(@ModelAttribute GuestbookVo guestboookVo) {
+			System.out.println("GuestbookController=add");
+			
+			guestbookDao.guestInsert(guestboookVo);
 		
-		guestbookDao.guestInsert(guestboookVo);
-	
-	return "redirect:./gList";
-}
-	
-//삭제-폼
-	@RequestMapping(value = "/deleteForm", method= {RequestMethod.GET, RequestMethod.POST})
-	public String deleteForm(@ModelAttribute GuestbookVo guestboookVo) {
-		System.out.println("GuestbookController=deleteForm");
-		
-	return "deleteForm";
-}
-//삭제
-	@RequestMapping(value = "/delete", method= {RequestMethod.GET, RequestMethod.POST})
-	public String delete(@ModelAttribute GuestbookVo guestboookVo) {
-		System.out.println("GuestbookController=delete");
-		
-		guestbookDao.guestbookDelete(guestboookVo);
-	
 		return "redirect:./gList";
-}
+		}
+		
+//삭제-폼
+		@RequestMapping(value = "/deleteForm", method= {RequestMethod.GET, RequestMethod.POST})
+		public String deleteForm(@ModelAttribute GuestbookVo guestboookVo) {
+			System.out.println("GuestbookController=deleteForm");
+			
+		return "deleteForm";
+		}
+//삭제
+		@RequestMapping(value = "/delete", method= {RequestMethod.GET, RequestMethod.POST})
+		public String delete(@ModelAttribute GuestbookVo guestboookVo) {
+			System.out.println("GuestbookController=delete");
+			
+			guestbookDao.guestbookDelete(guestboookVo);
+		
+			return "redirect:./gList";
+		}
 	
 
 }
